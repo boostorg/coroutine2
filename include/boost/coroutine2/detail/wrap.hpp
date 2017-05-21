bcb2b415ceda624ce9790f3aa4f4192e1b0813e3
Oliver Kowalke <oliver.kowalke@gmail.com>
2017-05-21 10:47:58 +0200

    call/cc supports ucontext_t and WinFiber

--- include/boost/coroutine2/detail/wrap.hpp
+++ include/boost/coroutine2/detail/wrap.hpp
@@ -11,11 +11,7 @@
 
 #include <boost/config.hpp>
 #include <boost/context/detail/invoke.hpp>
-#if (BOOST_EXECUTION_CONTEXT==1)
-# include <boost/context/execution_context.hpp>
-#else
-# include <boost/context/continuation.hpp>
-#endif
+#include <boost/context/continuation.hpp>
 
 #include <boost/fiber/detail/config.hpp>
 #include <boost/fiber/detail/data.hpp>
@@ -28,45 +24,6 @@ namespace boost {
 namespace coroutines2 {
 namespace detail {
 
-#if (BOOST_EXECUTION_CONTEXT==1)
-template< typename Fn1, typename Fn2  >
-class wrapper {
-private:
-    typename std::decay< Fn1 >::type    fn1_;
-    typename std::decay< Fn2 >::type    fn2_;
-    boost::context::execution_context   ctx_;
-
-public:
-    wrapper( Fn1 && fn1, Fn2 && fn2,
-             boost::context::execution_context const& ctx) :
-        fn1_( std::move( fn1) ),
-        fn2_( std::move( fn2) ),
-        ctx_{ ctx } {
-    }
-
-    wrapper( wrapper const&) = delete;
-    wrapper & operator=( wrapper const&) = delete;
-
-    wrapper( wrapper && other) = default;
-    wrapper & operator=( wrapper && other) = default;
-
-    void operator()( void * vp) {
-        boost::context::detail::invoke(
-                std::move( fn1_),
-                fn2_, ctx_, vp);
-    }
-};
-
-template< typename Fn1, typename Fn2  >
-wrapper< Fn1, Fn2 >
-wrap( Fn1 && fn1, Fn2 && fn2,
-      boost::context::execution_context const& ctx) {
-    return wrapper< Fn1, Fn2 >(
-            std::forward< Fn1 >( fn1),
-            std::forward< Fn2 >( fn2),
-            ctx);
-}
-#else
 template< typename Fn1, typename Fn2  >
 class wrapper {
 private:
@@ -101,7 +58,6 @@ wrap( Fn1 && fn1, Fn2 && fn2) {
             std::forward< Fn1 >( fn1),
             std::forward< Fn2 >( fn2) );
 }
-#endif
 
 }}}
 
